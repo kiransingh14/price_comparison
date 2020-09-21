865d33ee25f38d2a4efa133a59c5d23385bf1171
unknown <kiranshrinivassingh@gamil.com>
2020-09-21 16:13:34 +0530

    Added my project

--- views.py
+++ views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render , redirect
+from .models import *
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def registerPage(request):                                                  #create a registration form                                          
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form' : form}
+    return render(request , 'price_comparison/register.html' , context)
