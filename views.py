226edfb2e15edb28752529314facd657bf46614f
unknown <kiranshrinivassingh@gamil.com>
2020-09-24 20:30:17 +0530

    Added my project

--- views.py
+++ views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render , redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+
+from django.contrib import messages
+
 
 from .models import *
 
@@ -15,11 +19,23 @@ def registerPage(request):                                                  #cre
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request,'Account was created for '+ user)
     context = {'form' : form}
     return render(request , 'price_comparison/register.html' , context)
 
 
 
 def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username ,password=password) 
+        
+        if user is not None:
+            login(request,user)
+            return redirect('home')
     context={}
     return render(request , 'price_comparison/login.html',context)
