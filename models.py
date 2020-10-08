bebf6783e934a8433ebb4073fef7b5739d766c44
unknown <kiranshrinivassingh@gamil.com>
2020-10-08 19:37:03 +0530

    added my project

--- models.py
+++ models.py
@@ -4,9 +4,9 @@ from django import forms
 
 User = get_user_model()
 
-class find(models.Model):
-    searchitem=models.CharField(max_length=100)
+class shop(models.Model):
+    name=models.CharField(max_length=100)
     search=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.searchitem
+        return self.name
